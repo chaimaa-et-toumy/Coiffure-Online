a6065d7cb68715e0b7d1146ec70fc74e5f0d0f74
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2023-04-02 23:53:15 +0000

    protect route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ import Service from "./pages/dashboard/Service/service";
 import Rendez_vous from "./pages/dashboard/Rendez_vous/rendez_vous";
 import Commantaire from "./pages/dashboard/commantaire";
 import Home from "./pages/client/home"
+import ProtectRoute from "./Utils/ProtectRoute";
+
 
 
 function App() {
@@ -21,14 +23,17 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/forgotpassword" element={<Forgotpassword />} />
           <Route path="/resetpassword/:token" element={<Resetpassword />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/categorie" element={<Categorie />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/rendez-vous" element={<Rendez_vous />} />
-          <Route path="/commentaire" element={<Commantaire />} />
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
+
+          <Route element={<ProtectRoute />}  >
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/categorie" element={<Categorie />} />
+            <Route path="/service" element={<Service />} />
+            <Route path="/rendez-vous" element={<Rendez_vous />} />
+            <Route path="/commentaire" element={<Commantaire />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
